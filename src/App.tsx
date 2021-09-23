6dffd0d9562d3c00629a15d3acd22920fc51e88b
Amani Kiruga <amaniedu71@gmail.com>
2021-09-23 19:32:20 -0400

    Make initial layout without css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import CARDS from './assets/cards.json'
+import {ControlPanel} from './components/ControlPanel'
+import {CardViewer} from './components/CardViewer'
 import './App.css';
-
-function App() {
+function App() : JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ControlPanel></ControlPanel>
+        <CardViewer></CardViewer>
     </div>
   );
 }
---
+++ src/components/CardViewer.tsx
@@ -0,0 +1,7 @@
+export function CardViewer() : JSX.Element { 
+    return ( 
+        <div>
+            Card Viewer
+        </div>
+    )
+}
